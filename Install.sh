98b917df90f37768939a79b1aa1401dcc49ffdb5
Gregory Fuentes <gregoryfuentes80@gmail.com>
2021-05-15 13:23:56 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -30,7 +30,7 @@ apt-get install -y python3-venv
 echo -e "${GREEN}[*] Installing Pipx${NC}"
 xterm -e bash -c 'python3 -m pip install pipx; python3 -m pipx ensurepath'
 
-#Make bash default shell
+#Make Zsh default shell
 echo -e "${GREEN}[*] Change default shell to Zsh${NC}"
 chsh -s /bin/zsh
 
@@ -74,6 +74,9 @@ apt-get install -y zsh
 apt-get install -y nano
 apt-get install -y tmux
 
+#Install gnumeric for converting excel files
+apt-get -y install gnumeric
+
 #Need to uninstall
 #sudo rm /usr/bin/lsb_release
 
@@ -97,21 +100,13 @@ apt-get install seclists
 echo -e "${GREEN}[*] Installing Python SSL Module${NC}"
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-#Install fzf for CommandLine
-echo -e "${GREEN}[*] Installing FZF${NC}"
-apt-get install fzf
-
 #Install exa
 echo -e "${GREEN}[*] Installing EXA${NC}"
 apt install exa
 
-# enable command aliasing
-echo -e "${GREEN}[*] Enable Command Aliasing${NC}"
-shopt -s expand_aliases
-
 #Make folders
 cd /home/kali/Desktop/
-mkdir -p Bug
+mkdir -p HTB
 
 #Create Tools Dir
 cd ~/
@@ -129,6 +124,11 @@ python3 -m pipx install tldr.py
 #Install Impacket
 echo -e "${GREEN}[*] Installing Impacket${NC}"
 python3 -m pipx install git+https://github.com/SecureAuthCorp/impacket.git
+python3 -m pipx ensurepath
+
+#Install bloodhound
+echo -e "${GREEN}[*] Installing Bloodhound${NC}"
+python3 -m pipx install bloodhound
 python3 -m pipx ensurepath
 
 #Unzip Rockyou
@@ -154,28 +154,21 @@ echo -e "${GREEN}[*] Installing Nishang${NC}"
 cd ~/Tools
 git clone https://github.com/samratashok/nishang.git
 
-# masscan
-echo -e "${GREEN}[*] Installing masscan${NC}"
-cd ~/Tools
-apt-get install -y libpcap-dev
-git clone https://github.com/robertdavidgraham/masscan.git
-cd masscan
-make
-ln -sf ~/Tools/masscan/bin/masscan /usr/local/bin/masscan    
+#Add Scripts to Directory for PrivEsc 
+echo -e "${GREEN}[*] Installing Privesc Scripts${NC}"
+cd ~/Scripts
+git clone https://github.com/HarmJ0y/PowerUp.git
+git clone https://github.com/M4ximuss/Powerless.git
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/tree/master/winPEAS.git
+git clone https://github.com/rasta-mouse/Watson.git
+git clone https://github.com/bitsadmin/wesng.git
+git clone https://github.com/diego-treitos/linux-smart-enumeration.git
+git clone https://github.com/cwinfosec/revshellgen
 
-# dnsenum
-echo -e "${GREEN}[*] Installing dnsenum${NC}"
-apt-get install -y cpanminus 
-cd ~/Tools
-git clone https://github.com/fwaeytens/dnsenum.git 
-cd dnsenum/ 
-chmod +x dnsenum.pl 
-ln -s ~/Tools/dnsenum/dnsenum.pl /usr/bin/dnsenum 
-cpanm String::Random 
-cpanm Net::IP 
-cpanm Net::DNS 
-cpanm Net::Netmask
-cpanm XML::Writer
+#dont use option -e on suidenum
+mkdir -p SUID
+cd ~/Scripts/SUID
+wget https://raw.githubusercontent.com/Anon-Exploiter/SUID3NUM/master/suid3num.py --no-check-certificate && chmod 777 suid3num.py
 
 # massdns
 echo -e "${GREEN}[*] Installing massdns${NC}"
@@ -185,15 +178,6 @@ git clone https://github.com/blechschmidt/massdns.git
 cd massdns/
 make
 ln -sf ~/Tools/massdns/bin/massdns /usr/local/bin/massdns
-
-# altdns
-echo -e "${GREEN}[*] Installing altdns${NC}"
-cd ~/Tools 
-git clone https://github.com/infosec-au/altdns.git 
-cd altdns 
-pip3 install -r requirements.txt 
-chmod +x setup.py 
-python setup.py install
 
 # thc-hydra
 echo -e "${GREEN}[*] Installing thc-hydra${NC}"
@@ -206,41 +190,6 @@ git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r/
 pip3 install -r requirements.txt
 ln -s ~/Tools/Sublist3r/sublist3r.py /usr/local/bin/sublist3r
-
-# knock
-echo -e "${GREEN}[*] Installing Knockpy${NC}"
-apt-get install -y python-dnspython 
-cd ~/Tools
-git clone https://github.com/guelfoweb/knock.git
-cd knock
-chmod +x setup.py
-python setup.py install
-
-# teh_s3_bucketeers
-echo -e "${GREEN}[*] Installing teh_s3_bucketeers${NC}"
-cd ~/Tools
-git clone https://github.com/tomdev/teh_s3_bucketeers.git 
-cd teh_s3_bucketeers 
-chmod +x bucketeer.sh 
-ln -sf ~/Tools/teh_s3_bucketeers/bucketeer.sh /usr/local/bin/bucketeer
-
-# Recon-ng
-echo -e "${GREEN}[*] Installing Recon-ng${NC}"
-cd ~/Tools 
-git clone https://github.com/lanmaster53/recon-ng.git 
-cd recon-ng 
-pip3 install -r REQUIREMENTS 
-chmod +x recon-ng 
-ln -sf ~/Tools/recon-ng/recon-ng /usr/local/bin/recon-ng
-
-# XSStrike
-echo -e "${GREEN}[*] Installing XSStrike${NC}"
-cd ~/Tools
-git clone https://github.com/s0md3v/XSStrike.git 
-cd XSStrike 
-pip3 install -r requirements.txt 
-chmod +x xsstrike.py
-ln -sf ~/Tools/XSStrike/xsstrike.py /usr/local/bin/xsstrike
 
 # sqlmap
 echo -e "${GREEN}[*] Installing sqlmap${NC}"
@@ -278,58 +227,9 @@ apt-get install -y libwww-perl
 chmod +x joomscan.pl
 ln -sf ~/toolkit/joomscan/joomscan.pl /usr/local/bin/joomscan
 
-# commix 
-echo -e "${GREEN}[*] Installing commix${NC}"
-cd ~/Tools
-git clone https://github.com/commixproject/commix.git 
-cd commix 
-chmod +x commix.py 
-ln -sf ~/Tools/commix/commix.py /usr/local/bin/commix
-
-# dnsrecon
-echo -e "${GREEN}[*] Installing dnsrecon${NC}"
-apt-get install -y dnsrecon
-
-# virtual-host-discovery
-cd ~/Tools
-git clone https://github.com/AlexisAhmed/virtual-host-discovery.git 
-cd virtual-host-discovery 
-chmod +x scan.rb 
-ln -sf ~/Tools/virtual-host-discovery/scan.rb /usr/local/bin/virtual-host-discovery
-
-# theHarvester
-echo -e "${GREEN}[*] Installing theHarvester${NC}"
-cd ~/Tools
-git clone https://github.com/AlexisAhmed/theHarvester.git 
-cd theHarvester 
-python3 -m pip install -r requirements.txt 
-chmod +x theHarvester.py
-ln -sf ~/Tools/theHarvester/theHarvester.py /usr/local/bin/theharvester
-
-# CloudFlair
-echo -e "${GREEN}[*] Installing CloudFlair${NC}"
-cd ~/Tools
-git clone https://github.com/christophetd/CloudFlair.git 
-cd CloudFlair 
-pip3 install -r requirements.txt 
-chmod +x cloudflair.py 
-ln -sf ~/Tools/CloudFlair/cloudflair.py /usr/local/bin/cloudflair
-
-# bucket_finder
-echo -e "${GREEN}[*] Installing bucket_finder${NC}"
-cd ~/Tools
-git clone https://github.com/AlexisAhmed/bucket_finder.git 
-cd bucket_finder 
-chmod +x bucket_finder.rb 
-ln -sf ~/Tools/bucket_finder/bucket_finder.rb /usr/local/bin/bucket_finder
-
 # s3recon
 echo -e "${GREEN}[*] Installing s3recon${NC}"
 pip3 install setuptools pyyaml pymongo requests s3recon
-
-# subfinder
-echo -e "${GREEN}[*] Installing subfinder${NC}"
-go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
 
 # whatweb 
 echo -e "${GREEN}[*] Installing whatweb${NC}"
@@ -339,16 +239,7 @@ cd WhatWeb
 chmod +x whatweb
 ln -sf ~/Tools/WhatWeb/whatweb /usr/local/bin/whatweb
 
-# fierce
-echo -e "${GREEN}[*] Installing fierce${NC}"
-python3 -m pipx install fierce
-
-# amass
-echo -e "${GREEN}[*] Installing amass${NC}"
-export GO111MODULE=on
-go get -v github.com/OWASP/Amass/v3/...
-
-#Refresh bash profile before installing pyenv
+#Refresh Zsh profile before installing pyenv
 echo -e "${GREEN}[*] Reload Zsh profile${NC}"
 source ~/.zshrc
 
@@ -359,3 +250,14 @@ curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+#Init Metasploit
+systemctl start postgresql
+systemctl enable postgresql
+msfdb init
+
+#Install Chisel binaries
+cd ~/Tools
+git clone https://github.com/jpillora/chisel/releases/download/v1.7.4/chisel_1.7.4_linux_amd64.gz
+git clone https://github.com/jpillora/chisel/releases/download/v1.7.4/chisel_1.7.4_windows_amd64.gz
+
